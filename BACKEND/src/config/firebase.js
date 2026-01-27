66d6c8dae3460f885502b158a4011f37be24d6b9
Akshat <akshatindia2011@gmail.com>
2026-01-27 21:05:45 +0530

    just deploying

--- BACKEND/src/config/firebase.js
+++ BACKEND/src/config/firebase.js
@@ -1,12 +1,17 @@
 const admin = require("firebase-admin");
 const path = require("path");
 
-const serviceAccount = require(
-  path.join(__dirname, "../../serviceAccountKey.json")
-);
+// const serviceAccount = require(
+//   path.join(__dirname, "../../serviceAccountKey.json")
+// );
 
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+const { applicationDefault } = require('firebase-admin/app');
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: applicationDefault() 
 });
 
 module.exports = admin;
